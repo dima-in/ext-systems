e2c90a0ec653f59bf6a1f61b7a00f9dad10bf909
dima-in <53936500+dima-in@users.noreply.github.com>
2022-03-25 14:40:47 +0300

    Update Server.java

--- network/src/main/java/ex/java/net/Server.java
+++ network/src/main/java/ex/java/net/Server.java
@@ -7,50 +7,70 @@ import java.net.Socket;
 public class Server {
     public static void main(String[] args) throws IOException {
         ServerSocket socket = new ServerSocket(25225);
-        while (true) {
+        while (true){
+            // ожидает соединения
             Socket client = socket.accept();
-            //создать и запустить handleRequest в новый поток
+            // создать и запустить handleRequest в новый поток
             new SimpleServer(client).start();
         }
     }
 }
 class SimpleServer extends Thread {
+    // this.client:
     private Socket client;
 
-        public SimpleServer(Socket client){
-            this.client = client;
-        }
+    public SimpleServer(Socket client) {
+        this.client = client;
+    }
 
-        public void run() {
-            handleRequest();
-        }
+    public void run() {
+        handleRequest();
+    }
 
     private void handleRequest() {
         try {
             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-            StringBuilder stringBuilder = new StringBuilder("Good ");
+            // принимает запрос - строку
             String request = bufferedReader.readLine();
+            // полученная строка делится на 2 слова
             // \s+ один или больше space символов
-            // split возвращает String[]
-            // разделяет строку на массив слов, разделенных regexp
+            // split возвращает String[] разделяет строку
+            // на массив слов, разделенных regexp
             String[] lines = request.split("\\s+");
+
             String command = lines[0];
             String userName = lines[1];
-            System.out.println("server got string 1 : " + request);
-            Thread.sleep(2000);// 2 sec
 
-            stringBuilder.append(request);
-            bufferedWriter.write(stringBuilder.toString());
+            System.out.println("Server got string  1: " + command);
+            System.out.println("Server got string  2: " + userName);
+
+//            StringBuilder stringBuilder = new StringBuilder("Hello, ");
+//            stringBuilder.append(request);
+            String response = buildResponse(command, userName);
+            bufferedWriter.write(response);
             bufferedWriter.newLine();
             bufferedWriter.flush();
 
             bufferedWriter.close();
             bufferedReader.close();
             client.close();
+
+
         } catch (Exception e) {
             e.printStackTrace(System.out);
         }
     }
+    private String buildResponse(String command, String userName) {
+        switch (command){
+            case "HELLO" : return "Hello, " + userName;
+            case "DAY" : return "Good day, " + userName;
+            case "EVENING" : return "Good evening, " + userName;
+            case "MORNING" : return "Good morning, " + userName;
+            default: return "OMG! " + userName;
+        }
+    }
 }
+// используя reflection создаются нужные объекты и кладутся в map
+
