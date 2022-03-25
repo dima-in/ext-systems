c216b70c45979c7552dcfca9ce219c176253975a
dima-in <53936500+dima-in@users.noreply.github.com>
2022-03-25 13:48:58 +0300

    Update Client.java

--- network/src/main/java/ex/java/net/Client.java
+++ network/src/main/java/ex/java/net/Client.java
@@ -1,47 +1,48 @@
 package ex.java.net;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
-import java.time.LocalDateTime;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
-        for (int i=0; i<10; i++) {
-
+    public static void main(String[] args) {
+        for (int i=0; i<10; i++){
+            // i номер команды
             SimpleClient simpleClient = new SimpleClient(i);
             simpleClient.start();
         }
     }
 }
-class SimpleClient extends  Thread {
-    //массив команд длс сервера
-    private final static String[] COMMAND =
-            {"HELLO", "MORNING", "DAY", "EVENING"};
+class SimpleClient extends Thread {
+    //массив команд для сервера
+    private final static String[] COMMAND = {"HELLO", "MORNING", "DAY", "EVENING"};
 
     // добавить произвольное слово к отсылаемой строке
-
-    // this.number
+    // this.cmdNumber:
     private int cmdNumber;
-    // cmdNumber
+    // конструирует объект simpleClient
+    // в конструктор передается номер команды
     public SimpleClient(int cmdNumber){
         this.cmdNumber = cmdNumber;
     }
 
     @Override
     public void run() {
-        try
-        {
-            // простой сокет
-            Socket socket = new Socket("127.0.0.1", 25225);
+        try {
+            //простой сокет, host, port
+            Socket socket = new Socket("127.0.0.1",25225);
 
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            // чтобы не выйти за пределы массива
             // индекс элемента массива ровняется остатку от деления
             // cmdNumber < COMMAND.length остаток равен cmdNumber
             String command = COMMAND[cmdNumber % COMMAND.length];
 
             String sb = command + " " + "Dima";
-            // отправить command + Dima на сервер во внешний поток
+            // отправить command + Dima серверу во внешнем потоке
             bufferedWriter.write(sb);
             bufferedWriter.newLine();
             // отправить
@@ -53,8 +54,9 @@ class SimpleClient extends  Thread {
             bufferedReader.close();
             bufferedWriter.close();
 
-        } catch (Exception e) {
+        } catch (Exception e){
             e.printStackTrace(System.out);
         }
     }
+
 }
